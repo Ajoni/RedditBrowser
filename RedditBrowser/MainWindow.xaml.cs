1be5d2867d75ef55f29bae13a6edeed205222ee6
Adam Sienkiewicz <adam584@onet.pl>
2018-09-09 19:29:28 +0200

    You can press next for a meme and another one... but thats about it.

--- RedditBrowser/MainWindow.xaml.cs
+++ RedditBrowser/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RedditBrowser;
+using RedditSharp;
+using RedditSharp.Things;
 
 namespace RedditBrowser
 {
@@ -20,9 +23,49 @@ namespace RedditBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string subredditName { get; set; }
+        public Subreddit subreddit { get; set; }
+        int postNr { get; set; } = -1;
+        public IEnumerator<Post> it { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SelectSubDialog dialog = new SelectSubDialog();
+            dialog.ShowDialog();
+            if (dialog.DialogResult != true)
+            {
+                //add some sort of msg to gui here
+                return;
+            }
+            subredditName = dialog.subName;
+            subreddit = new Reddit().GetSubreddit($"/r/{subredditName}");
+            var posts = subreddit.Posts;
+            it = posts.GetEnumerator();
+            loadNextImg();
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            loadNextImg();
+        }
+
+        private void loadNextImg()
+        {
+            it.MoveNext(); postNr++;
+            statusLabel.Content = "Skipping non jpg posts";
+            while (it.Current.Url.ToString().Contains(".jpg") != true)
+            {
+                it.MoveNext(); postNr++;
+            }
+            statusLabel.Content = "Loading img";
+            string source = it.Current.Url.ToString();
+            meme.Source = new BitmapImage(new Uri(source, UriKind.Absolute));
+            statusLabel.Content = "";
         }
     }
 }
