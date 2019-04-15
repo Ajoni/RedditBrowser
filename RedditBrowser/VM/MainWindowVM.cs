6ed9de5a48a1ed5faa10430749759f992a748bc0
Ajoni <adam584@onet.pl>
2019-04-15 18:45:52 +0200

    troche bindowania

--- RedditBrowser/VM/MainWindowVM.cs
+++ RedditBrowser/VM/MainWindowVM.cs
@@ -3,6 +3,7 @@ using RedditBrowser.Classes;
 using RedditSharp.Things;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace RedditBrowser.VM
 								public Subreddit Subreddit { get; set; }
 								public bool Busy { get; set; }
 								public Person Person { get; set; }
+								public ObservableCollection<string> PossibleSubs { get; set; }
 								private Manager Manager { get; set; }
 				}
 }
