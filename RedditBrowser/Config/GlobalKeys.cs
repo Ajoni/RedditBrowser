ddb128a42a0c7e67980a7ca151deae977c2b137b
Mayster911 <krzysztofsosna@wp.pl>
2019-04-25 14:59:07 +0200

    Added basic configs

---
+++ RedditBrowser/Config/GlobalKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBrowser.Config
+{
+    public static class GlobalKeys
+    {
+        public static readonly string KeyConfigPath = "KeyConfigPath";
+    }
+}
--- RedditBrowser/MainWindow.xaml.cs
+++ RedditBrowser/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Logic;
+using Newtonsoft.Json;
+using RedditBrowser.Config;
 using RedditBrowser.Helpers;
 using RedditBrowser.ViewModel;
 using RedditSharp;
@@ -24,20 +26,32 @@ namespace RedditBrowser
 {
     public partial class MainWindow : Window
     {
-								MainViewModel VM = new MainViewModel();
+        MainViewModel VM;
 
         public MainWindow()
         {
             InitializeComponent();
-
-												//VM.Subreddit= new Reddit().GetSubreddit($"/r/Animemes");
-												this.DataContext = VM;
         }
 
-								private async void WindowMain_Loaded(object sender, RoutedEventArgs e)
-								{
-												await this.VM.Init();
-								}
-				}
+        private async void WindowMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitConfig();
+
+            VM = new MainViewModel();
+            this.DataContext = VM;
+            await this.VM.Init();
+        }
+
+        private void InitConfig()
+        {
+            var configFile = GlobalPaths.Config;
+            if (!File.Exists(configFile))
+                return;
+
+            string json = File.ReadAllText(configFile);
+            var config = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            GlobalConfig.Set(config);
+        }
+    }
 
 }
