9b7ad9ec5154f06550a4bec24842c543f1a9b7b1
Mayster911 <krzysztofsosna@wp.pl>
2019-04-28 21:04:01 +0200

    Minor graphical changes

--- RedditBrowser/ViewModel/TopPanelVM.cs
+++ RedditBrowser/ViewModel/TopPanelVM.cs
@@ -7,13 +7,15 @@ namespace RedditBrowser.ViewModel
 {
 	public class TopPanelVM : IViewModel
 	{
+        private string _SubredditName;
+
 		public string Header { get; set; }
-		public string SubredditName { get; set; }
+		public string SubredditName { get { return "r/" + _SubredditName; } set { _SubredditName = value; } }
 		public string Search { get; set; }
 
 		public ICommand GoToListView => new DelegateCommand((a) =>
-																																																	{
-																																																		Messenger.Default.Send(new GoToListViewMessage());
-																																																	});
+		{
+			Messenger.Default.Send(new GoToListViewMessage());
+		});
 	}
 }
