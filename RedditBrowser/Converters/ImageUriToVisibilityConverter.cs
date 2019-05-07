ac2feb11943cf96a543747293a4905492d07db4f
Mayster911 <krzysztofsosna@wp.pl>
2019-05-07 15:47:25 +0200

    Comments button, other changes

---
+++ RedditBrowser/Converters/ImageUriToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RedditBrowser.Converters
+{
+    public class ImageUriToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Uri uri = (Uri)value;
+            if (!uri.IsAbsoluteUri)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
